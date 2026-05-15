763ebd8e0859af0a629eaa543894e919d48bdc62
krupali <krupali123dabhi@gmail.com>
2026-05-15 17:41:46 +0530

    changed title

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center mb-4">
-                <h1>Patients Management</h1>
+                <h1>Patients</h1>
                 <a href="{{ route('patients.create') }}" class="btn btn-primary">
                     <i class="fas fa-plus"></i> Add New Patient
                 </a>
@@ -68,7 +68,7 @@
                             </tbody>
                         </table>
                     </div>
-                    
+
                     <div class="d-flex justify-content-center">
                         {{ $patients->links() }}
                     </div>
